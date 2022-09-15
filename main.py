081ae887fc53db3d3004a024723ee513a448c41b
shamiiow <ishihlanzo@gmail.com>
2022-09-15 22:55:11 +0200

    il me manque plus que le mélange des lettres

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ root.geometry("300x300")
 root.title('Find the word !')
 
 
-def test() :
-
-    if word.cget('text') == "":
+def test():
+    rematch = False
+    if find.cget('text') == "Mélanger":
         word.config(text=user_try.get())
-    else:
-        if user_try.get() == word.cget('text') :
-            win.config(text='Gagné')
+        user_try.delete(0, tkinter.END)
+        find.config(text='jouer')
+
+    elif user_try.get() == word.cget('text'):
+        find.config(text='Gagné')
+    elif user_try.get() != word.cget('text'):
+        word.config(text='Perdu : '+word.cget('text'))
+        rematch = True
+    if rematch:
+        user_try.delete(0, tkinter.END)
+        find.config(text="Mélanger")
 
 
 word = tkinter.Label(root,
                      text="")
 user_try = tkinter.Entry(root)
 find = tkinter.Button(root,
-                      text="Are you right ?",
+                      text="Mélanger",
                       command=test)
 win = tkinter.Label(root,
                     text="")
